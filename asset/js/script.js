f01580eb33ea271d04ec613fc0a7db013876211b
FabioVasi <vasifabio96@gmail.com>
2023-08-30 14:22:13 +0200

    add shoppinglist by while cycle

--- asset/js/script.js
+++ asset/js/script.js
@@ -5,3 +5,32 @@ Consigli:
 Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
 Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
 */
+
+const shoppingList = []
+
+console.log(shoppingList);
+
+const ulEl = document.querySelector('ul');
+
+let i = 0;
+
+while (i < 5) {
+    
+    const elementList = prompt("type ingredient's of your shopping list here");
+
+    console.log(elementList);
+
+    shoppingList.push(elementList);
+    
+    const liEl = document.createElement('li');
+    
+    liEl.append(elementList);
+
+    console.log(liEl);
+
+    ulEl.append(liEl);
+
+    i++
+    
+}
+
