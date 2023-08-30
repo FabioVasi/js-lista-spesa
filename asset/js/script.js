0deb7c9f4734fd60b11c4169d691c7b7ce7a201b
FabioVasi <vasifabio96@gmail.com>
2023-08-30 18:34:15 +0200

    fix

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,4 +1,5 @@
 /*
+
 Consegna:
 Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
 Consigli:
@@ -24,16 +25,20 @@ const shoppingList = []
 console.log(shoppingList);
 // creo una costante dentro la quale voglio vedere stampati in pagina gli elementi della lista in pagina
 const ulEl = document.querySelector('ul');
-// ciclo while
+// creo un ciclo while dentro il quale chiedo all'utente di inserire dei dati x numero di volte che verranno stampate in console e pagina fino al completamento del ciclo
 let i = 0;
 
 while (i < 5) {
     
+    console.log(i);
+
     const elementList = prompt("type ingredient's of your shopping list here");
 
     console.log(elementList);
 
-    if(elementList === null) {
+    if(elementList == null || elementList == "") {
+
+        alert('please type something');
 
         i--
 
@@ -54,4 +59,3 @@ while (i < 5) {
     ulEl.append(liEl);
 
 }
-
